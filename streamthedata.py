9f11cbfdb7ff2beb24cdfcdf8a538139f0cb57fd
hazeleyesandsilverhairs <shapesandstories@gmail.com>
2022-08-13 14:44:17 +0300

    Date slider added and some text updates

--- streamthedata.py
+++ streamthedata.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-import datetime
+from datetime import datetime
 st.set_page_config(layout="wide") # call page_config directly after the imports.
 
 st.title('Visualize Booking Data')
@@ -31,8 +31,8 @@ data_load_state.text('Loading data...done!')
 st.subheader('Raw data')
 st.write(df)
 # Displaying raw data
-
-# Filter by platform options starts
+############################################################
+# Filter by platform options
 st.subheader('Filtering by Platform')
 platformOptions =  df['Platform'].unique()
 
@@ -44,9 +44,9 @@ setPlatformOptions)
 st.write('You selected:', option)
 
 st.write(df[df['Platform'] == option])
-# Filter by platform options ends 
-
-# Filter by property options starts
+# Filter by platform options 
+############################################################
+# Filter by property options
 st.subheader('Filtering by Property')
 platformOptions =  df['Property'].unique()
 
@@ -58,14 +58,13 @@ setPlatformOptions)
 st.write('You selected:', option)
 
 st.write(df[df['Property'] == option])
-# Filter by platform options ends 
-
-
-# Selecting a date range filtering starts 
+# Filter by platform options 
+############################################################
+# Selecting a date range filtering 
 st.subheader('Filtering by Date')
 
-min_date = datetime.datetime(2022,1,1)
-max_date = datetime.date(2022,9,1)
+min_date = datetime(2022,1,1)
+max_date = datetime(2022,9,1)
 
 st.write('Select first and last date (first date should be earlier than the last date)')
 first_date = st.sidebar.date_input("Pick first date",min_value=min_date, max_value=max_date) # 
@@ -74,4 +73,19 @@ st.write("You selected as first date:", first_date)
 second_date = st.sidebar.date_input("Pick second date",min_value=min_date, max_value=max_date) # 
 st.write("You selected as second date:", second_date)
 
-st.write(df[(df['Created at (lead time to start date)']>first_date) & (df['Created at (lead time to start date)']< second_date)]  )
+st.write(df[(df['Created at (lead time to start date)']>first_date) & (df['Created at (lead time to start date)']< second_date)])
+# Selecting a date range filtering
+############################################################
+# Selecting a date range using slider
+st.write('Select first and last date using slider')
+sliderDate = st.sidebar.slider("Pick first date", value=(min_date, max_date), format="YY/MM/DD") #
+
+first_date_slider = sliderDate[0]
+second_date_slider = sliderDate[1]
+
+st.write("You selected as first date by using slider:", first_date_slider)
+st.write("You selected as second date by using slider:", second_date_slider)
+
+st.write(df[(df['Created at (lead time to start date)']>first_date_slider) & (df['Created at (lead time to start date)']< second_date_slider)])
+# Selecting a date range using slider
+############################################################
